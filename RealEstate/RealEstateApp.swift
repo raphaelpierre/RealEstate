e71bdc211f8f0737bceaaf08b612128edae910d9
Raphaël Pierre <pierre.raphael@gmail.com>
2025-01-15 15:16:12 +0100

    Initial Commit

---
+++ RealEstate/RealEstateApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealEstateApp.swift
+//  RealEstate
+//
+//  Created by Raphael PIERRE on 15.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct RealEstateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
